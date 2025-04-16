c0d01cfe0a5eb359eb03ff2a31d4c546df0c92bb
SamAllen06 <samgallen06@gmail.com>
2025-04-16 10:34:04 -0400

    created Budgeter class

---
+++ Budgeter.java
@@ -0,0 +1,21 @@
+class Budgeter{
+	HeaderLinkedList headerList;
+
+	public void Budgeter(){
+		this.headerList = new HeaderLinkedList;
+	}//end constructor def
+
+	public void setHeaderList(HeaderLinkedList list){
+		this.headerList = list;
+	}//end setter
+
+	public HeaderLinkedList getHeaderList(){
+		return this.headerList;
+	}//end getter
+
+	public String menu(){
+		String response = "";
+		Scanner input = new Scanner(System.in);
+		System.out.print("
+	}//end menu def
+}//end class def
